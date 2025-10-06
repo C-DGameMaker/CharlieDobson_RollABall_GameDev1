1d16d8af71c925ecb33654c7b1466758d77fa319
C-DGameMaker <charlotte5634@outlook.com>
2025-10-06 15:26:39 -0300

    StartedPauseMenu

---
+++ Assets/Scripts/PauseMenuManager.cs
@@ -0,0 +1,53 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuManager : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if(isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                PauseMenu();
+            }
+        }
+    }
+
+    void PauseMenu()
+    {
+        Time.timeScale = 0;
+        isPaused = true;
+        pauseMenuUI.SetActive(true);
+    }
+
+    public void OnResumeButton()
+    {
+        Resume();
+    }
+
+    public void OnRestartButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void OnQuitButton()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    void Resume()
+    {
+        Time.timeScale = 1;
+        isPaused = false;
+        pauseMenuUI.SetActive(false);
+    }
+}
